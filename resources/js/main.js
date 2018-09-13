7d5132c1eb67f8da10dbe64b817b63a82771a468
dancs2520 <dancs2520@gmail.com>
2018-09-13 17:34:58 -0300

    apukas files

---
+++ resources/js/main.js
@@ -0,0 +1,10 @@
+setTimeout(()=>{
+    $('#apukaModal').modal('show');
+}, 1)
+
+
+//settings for the parallax.js effect on the main banner
+var scene = document.getElementById('scene1');
+var parallaxInstance = new Parallax(scene);
+
+
